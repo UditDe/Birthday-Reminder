4e07a24eb3033f21e825192720cb4660ea22712b
Udit <uditde.com@gmail.com>
2022-12-22 22:41:12 +0530

    first prototype is ready

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import data from "./data";
 import List from "./List";
 
 function App() {
-  const clearAll = () => {};
+  const [people, setPeople] = useState(data);
+  const clearAll = () => {
+    setPeople([]);
+  };
   return (
     <>
       <main>
         <section className="container">
-          <h3>Bday Zero</h3>
-          <List />
+          <h3>{people.length} Birthdays Today </h3>
+          <List people={people}/>
           <button className="btn" onClick={clearAll}>
             Clear All
           </button>
--- src/List.js
+++ src/List.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const List = ({ people }) => {
   return (
     <>
-      <h2>List Component</h2>
+      {people.map((member) => {
+        const { id, name, image, age } = member;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years old</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
